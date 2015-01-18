ad62ad5b5c03fffdc88f41ba2bbbd95d5042d024
Tomás Martins <tomasmartins@me.com>
2015-01-18 21:44:47 +0000

    First changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("GMC");
+    case mBTC: return QString("mGMC");
+    case uBTC: return QString::fromUtf8("μGMC");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("GameCoin");
+    case mBTC: return QString("Milli-GameCoin (1 / 1,000)");
+    case uBTC: return QString("Micro-GameCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
