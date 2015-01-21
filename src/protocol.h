81a17f99f418212b8efede666b2f6da2327000fd
Tomás Martins <tomasmartins@me.com>
2015-01-21 17:06:34 +0000

    u.u

--- src/protocol.h
+++ src/protocol.h
@@ -19,7 +19,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 11068 : 11067;
+    return testnet ? 143605 : 11067;
 }
 
 
